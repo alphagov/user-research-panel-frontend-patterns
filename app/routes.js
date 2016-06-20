48bc5af23f6e4cdd24d50d0d93618c161ae19cb1
Tom Byers <tombaromba@gmail.com>
2016-06-20 11:23:34 +0100

    Add index and pages for text fields

--- app/routes.js
+++ app/routes.js
@@ -1,9 +1,26 @@
 module.exports = {
   bind : function (app, assetPath) {
-    // patterns - apply 
+    // patterns - index 
+    app.get('/', function (req, res) {
+      var page_name = "Index";
+      res.render('index', { 'page_name' : page_name });
+    });
+    // patterns - apply
     app.get('/apply', function (req, res) {
       var page_name = "Apply";
       res.render('apply', { 'page_name' : page_name });
+    });
+
+    // patterns - text fields - single text field 
+    app.get('/text-field-single', function (req, res) {
+      var page_name = "Single text field";
+      res.render('text-field-single', { 'page_name' : page_name });
+    });
+
+    // patterns - text fields - multiple text fields 
+    app.get('/text-field-multiple', function (req, res) {
+      var page_name = "Multiple text fields";
+      res.render('text-field-multiple', { 'page_name' : page_name });
     });
 
   }
