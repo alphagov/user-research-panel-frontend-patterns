431db993d3c6da599f2a627b69b95d8aee80bbba
Tom Byers <tombaromba@gmail.com>
2016-06-24 18:02:15 +0100

    Remove latest-release branch from process

--- scripts/push_release.sh
+++ scripts/push_release.sh
@@ -16,6 +16,3 @@ git push origin gh-pages
 printf "Creating new tag: $VERSION\n"
 git tag $VERSION
 git push origin $VERSION
-
-# Alias branch for the most recently released tag, for easier diffing
-git push -f origin master:latest-release
